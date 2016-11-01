79568ea0fefa91e23cae0d6b5aa27414dbe3188f
Andrew <apouleson@gmail.com>
2016-11-01 15:39:18 -0400

    Implemented using built in SVD

--- a2.scala
+++ a2.scala
@@ -22,22 +22,58 @@ object Assign2 {
         val missingfile = args(1)
         val outfile = args(2)
 
-        val smallRowLength = 504
-        val smallColLength = 24
+        val rowLength = 504
+        val colLength = 24
+
+        if(datafile == "small.csv") {
+          val rowLength = 504
+          val colLength = 24
+        } else if(datafile == "medium.csv") {
+          val rowLength = 52578
+          val colLength = 159
+        } else if(datafile == "large.csv") {
+          val rowLength = 18988
+          val colLength = 1036
+        } else {
+          val rowLength = 504
+          val colLength = 24
+        }
 
         val ofile = new File(outfile)
         val output = new BufferedWriter(new FileWriter(ofile))
+
+        var conf = new SparkConf().setAppName("Assign2")
+        var sc = new SparkContext(conf)
 
         //distributed matrix factorization
 
         val trainSet = readInDatePoints(datafile)
 
-        val R = Array.fill[Array[Double]](smallRowLength)(Array.fill[Double](smallColLength)(0.0))
+        var R = Array.fill[Array[Double]](rowLength)(Array.fill[Double](colLength)(0.0))
 
-        trainSet.map(t => {
-            R(t._1)(t._2) = t._3
+        trainSet.foreach( t => {
+          R(t._1)(t._2) = t._3
         })
 
+        val rows = sc.parallelize(R.map(row => Vectors.dense(row)))
+
+        val rowMatrix = new RowMatrix(rows)
+
+        val svd = rowMatrix.computeSVD(5, true)
+
+        val U = svd.U
+        val s = Matrices.diag(svd.s)
+        val V = svd.V
+
+        val A = U.multiply(s).multiply(V.transpose).rows.collect.zipWithIndex
+
+        A.foreach(t => {
+          var row = t._2
+          var vec = t._1
+          for(i <- 0 until colLength) {
+            output.write(row+","+i+","+vec(i)+"\n")
+          }
+        })
 
         //output.write(x._1+","+x._2+","+x._3+"\n") //need to write out values to missing coordinates
 
